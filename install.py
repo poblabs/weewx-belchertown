71310abe24116474801f900deb43628e9dff76ad
Pat O'Brien <pat@obrienphoto.net>
2018-09-05 12:53:37 -0400

    bump installer version

--- install.py
+++ install.py
@@ -9,7 +9,7 @@ def loader():
 class ExfoliationInstaller(ExtensionInstaller):
     def __init__(self):
         super(ExfoliationInstaller, self).__init__(
-            version="0.1",
+            version="0.6",
             name='Belchertown',
             description='A clean modern skin with real time streaming updates and interactive charts. Modeled after BelchertownWeather.com',
             author="Pat OBrien",
