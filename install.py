c1082de2389382b77b50831b80e1a8debade5ce6
Pat O'Brien <pat@obrienphoto.net>
2018-09-05 19:43:27 -0400

    Update installer

--- install.py
+++ install.py
@@ -9,7 +9,7 @@ def loader():
 class ExfoliationInstaller(ExtensionInstaller):
     def __init__(self):
         super(ExfoliationInstaller, self).__init__(
-            version="0.6",
+            version="0.7",
             name='Belchertown',
             description='A clean modern skin with real time streaming updates and interactive charts. Modeled after BelchertownWeather.com',
             author="Pat OBrien",
@@ -18,61 +18,74 @@ class ExfoliationInstaller(ExtensionInstaller):
                 'StdReport': {
                     'Belchertown': {
                         'skin':'Belchertown',
-                        'HTML_ROOT':'belchertown'}}},
-            files=[('bin/user', 
-                    ['bin/user/belchertown.py',
-                    'bin/user/belchertown_highchartsSearchX.py']),
-                   ('skins/Belchertown',
-                    ['skins/Belchertown/favicon.ico',
-                     'skins/Belchertown/footer.html.tmpl',
-                     'skins/Belchertown/header.html.tmpl',
-                     'skins/Belchertown/index.html.tmpl',
-                     'skins/Belchertown/about.inc.example',
-                     'skins/Belchertown/records.inc.example',
-                     'skins/Belchertown/skin.conf',
-                     'skins/Belchertown/style.css']),
-                   ('skins/Belchertown/about',
-                     ['skins/Belchertown/about/index.html.tmpl']),
-                   ('skins/Belchertown/graphs',
-                     ['skins/Belchertown/graphs/index.html.tmpl']),
-                   ('skins/Belchertown/NOAA',
-                    ['skins/Belchertown/NOAA/NOAA-YYYY-MM.txt.tmpl',
-                     'skins/Belchertown/NOAA/NOAA-YYYY.txt.tmpl']),
-                   ('skins/Belchertown/records',
-                     ['skins/Belchertown/records/index.html.tmpl']),
-                   ('skins/Belchertown/reports',
-                     ['skins/Belchertown/reports/index.html.tmpl']),
-                   ('skins/Belchertown/js',
-                     ['skins/Belchertown/js/highcharts-dayplots.js.tmpl',
-                     'skins/Belchertown/js/highcharts-weekplots.js.tmpl',
-                     'skins/Belchertown/js/highcharts-monthplots.js.tmpl',
-                     'skins/Belchertown/js/highcharts-yearplots.js.tmpl',
-                     'skins/Belchertown/js/index.html',
-                     'skins/Belchertown/js/responsive-menu.js']),
-                   ('skins/Belchertown/json',
-                     ['skins/Belchertown/json/day.json.tmpl',
-                     'skins/Belchertown/json/week.json.tmpl',
-                     'skins/Belchertown/json/month.json.tmpl',
-                     'skins/Belchertown/json/year.json.tmpl',
-                     'skins/Belchertown/json/index.html',
-                     'skins/Belchertown/json/weewx_data.json.tmpl']),
-                   ('skins/Belchertown/images',
-                     ['skins/Belchertown/images/clear-day.png',
-                     'skins/Belchertown/images/clear-night.png',
-                     'skins/Belchertown/images/cloudy.png',
-                     'skins/Belchertown/images/fog.png',
-                     'skins/Belchertown/images/hail.png',
-                     'skins/Belchertown/images/partly-cloudy-day.png',
-                     'skins/Belchertown/images/partly-cloudy-night.png',
-                     'skins/Belchertown/images/rain.png',
-                     'skins/Belchertown/images/sleet.png',
-                     'skins/Belchertown/images/snow.png',
-                     'skins/Belchertown/images/snowflake-icon-15px.png',
-                     'skins/Belchertown/images/sunrise.png',
-                     'skins/Belchertown/images/sunset.png',
-                     'skins/Belchertown/images/thunderstorm.png',
-                     'skins/Belchertown/images/tornado.png',
-                     'skins/Belchertown/images/wind.png',
-                     'skins/Belchertown/images/index.html'])
+                        'HTML_ROOT':'belchertown'
+                    },
+                    'Belchertown_Highcharts': {
+                        'skin':'Belchertown_Highcharts',
+                        'HTML_ROOT':'belchertown'
+                    }
+                }
+            },
+            files=[('bin/user', ['bin/user/belchertown.py',
+                                 'bin/user/belchertown_highchartsSearchX.py'
+                                ]
+                    ),
+                   ('skins/Belchertown', ['skins/Belchertown/favicon.ico',
+                                          'skins/Belchertown/footer.html.tmpl',
+                                          'skins/Belchertown/header.html.tmpl',
+                                          'skins/Belchertown/index.html.tmpl',
+                                          'skins/Belchertown/about.inc.example',
+                                          'skins/Belchertown/records.inc.example',
+                                          'skins/Belchertown/skin.conf',
+                                          'skins/Belchertown/style.css'
+                                         ]
+                    ),
+                   ('skins/Belchertown/about', ['skins/Belchertown/about/index.html.tmpl']),
+                   ('skins/Belchertown/graphs', ['skins/Belchertown/graphs/index.html.tmpl']),
+                   ('skins/Belchertown/NOAA', ['skins/Belchertown/NOAA/NOAA-YYYY-MM.txt.tmpl',
+                                               'skins/Belchertown/NOAA/NOAA-YYYY.txt.tmpl'
+                                              ]
+                    ),
+                   ('skins/Belchertown/records', ['skins/Belchertown/records/index.html.tmpl']),
+                   ('skins/Belchertown/reports', ['skins/Belchertown/reports/index.html.tmpl']),
+                   ('skins/Belchertown/js', ['skins/Belchertown/js/highcharts-dayplots.js.tmpl',
+                                             'skins/Belchertown/js/highcharts-weekplots.js.tmpl',
+                                             'skins/Belchertown/js/highcharts-monthplots.js.tmpl',
+                                             'skins/Belchertown/js/highcharts-yearplots.js.tmpl',
+                                             'skins/Belchertown/js/index.html',
+                                             'skins/Belchertown/js/responsive-menu.js'
+                                            ]
+                    ),
+                   ('skins/Belchertown/json', ['skins/Belchertown/json/index.html',
+                                               'skins/Belchertown/json/weewx_data.json.tmpl'
+                                              ]
+                    ),
+                   ('skins/Belchertown/images', ['skins/Belchertown/images/clear-day.png',
+                                                 'skins/Belchertown/images/clear-night.png',
+                                                 'skins/Belchertown/images/cloudy.png',
+                                                 'skins/Belchertown/images/fog.png',
+                                                 'skins/Belchertown/images/hail.png',
+                                                 'skins/Belchertown/images/partly-cloudy-day.png',
+                                                 'skins/Belchertown/images/partly-cloudy-night.png',
+                                                 'skins/Belchertown/images/rain.png',
+                                                 'skins/Belchertown/images/sleet.png',
+                                                 'skins/Belchertown/images/snow.png',
+                                                 'skins/Belchertown/images/snowflake-icon-15px.png',
+                                                 'skins/Belchertown/images/sunrise.png',
+                                                 'skins/Belchertown/images/sunset.png',
+                                                 'skins/Belchertown/images/thunderstorm.png',
+                                                 'skins/Belchertown/images/tornado.png',
+                                                 'skins/Belchertown/images/wind.png',
+                                                 'skins/Belchertown/images/index.html'
+                                                ]
+                    ),
+                   ('skins/Belchertown_Highcharts', ['skins/Belchertown_Highcharts/skin.conf']
+                    ),
+                   ('skins/Belchertown_Highcharts/json', ['skins/Belchertown_Highcharts/json/day.json.tmpl',
+                                                          'skins/Belchertown_Highcharts/json/week.json.tmpl',
+                                                          'skins/Belchertown_Highcharts/json/month.json.tmpl',
+                                                          'skins/Belchertown_Highcharts/json/year.json.tmpl',
+                                                         ]
+                    )
                    ]
-            )
+        )
