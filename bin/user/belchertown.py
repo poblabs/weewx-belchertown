f3534ee75aba4836d6686d842b0267d68aa6c7b5
Pat <pat@obrienphoto.net>
2019-05-15 09:46:55 -0400

    1.0rc9.1

--- bin/user/belchertown.py
+++ bin/user/belchertown.py
@@ -51,7 +51,7 @@ def logerr(msg):
     logmsg(syslog.LOG_ERR, msg)
     
 # Print version in syslog for easier troubleshooting
-VERSION = "1.0rc9"
+VERSION = "1.0rc9.1"
 loginf("version %s" % VERSION)
 
 class getData(SearchList):
