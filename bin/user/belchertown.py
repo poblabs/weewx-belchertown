835af5e42a3be5c233722088846814de32e56517
Pat <pat@obrienphoto.net>
2019-05-31 14:52:56 -0400

    Prepare for 1.0 release

--- bin/user/belchertown.py
+++ bin/user/belchertown.py
@@ -59,7 +59,7 @@ def logerr(msg):
     logmsg(syslog.LOG_ERR, msg)
     
 # Print version in syslog for easier troubleshooting
-VERSION = "1.0rc9.3"
+VERSION = "1.0"
 loginf("version %s" % VERSION)
 
 class getData(SearchList):
