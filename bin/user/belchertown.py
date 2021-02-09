06ffb6d3a17fd7184f14208bce31ded21c5a9fdc
norecords <radio@norecords.org>
2021-02-09 21:07:35 +0100

    Set mindepth to 2 to ignore quarry blast

--- bin/user/belchertown.py
+++ bin/user/belchertown.py
@@ -1694,8 +1694,8 @@ class getData(SearchList):
                 )
             elif self.generator.skin_dict["Extras"]["earthquake_server"] == "ReNaSS":
                 earthquake_url = (
-                    # Change mindepth to -1 if you want to show carrers shots
-                    "https://renass.unistra.fr/fdsnws/event/1/query?latitude=%s&longitude=%s&maxradius=%.2f&orderby=time&format=json&limit=1&mindepth=1"
+                    # Change mindepth to -1 if you want to show quarry blast
+                    "https://renass.unistra.fr/fdsnws/event/1/query?latitude=%s&longitude=%s&maxradius=%.2f&orderby=time&format=json&limit=1&mindepth=2"
                     % (latitude, longitude, int(earthquake_maxradiuskm) / 111.25)
                 )
             earthquake_is_stale = False
